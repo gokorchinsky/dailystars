a42fba532adc01246f472b962c2e836c03c36696
gokorchinsky <140883927+gokorchinsky@users.noreply.github.com>
2023-07-29 14:16:21 +0300

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "http://en.dailystars.net/graphql1";
+	const endpoint = "https://en.dailystars.net/graphql1";
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					'http://en.dailystars.net/' + encodeURI(path as string)
+					'https://en.dailystars.net/' + encodeURI(path as string)
 				}`,
 			},
 		};
